feda7f2dab890af42d0b7b7b06537d524c805bd1
TecHeReTiC <talex73@mail.ru>
2022-04-04 19:26:19 +0500

    Basic app loot has been implemented.

--- numeric_converter.py
+++ numeric_converter.py
@@ -2,10 +2,17 @@ import PySimpleGUI as sg
 
 layout = [
     [sg.Text('Hello, Insert value:'), sg.Input()],
-    [sg.Button('Convert!')],
-    [sg.Text('Result: ')]
+    [sg.Button('Convert!'), sg.Spin(['item1', 'item2'])],
+    [sg.Text('Result: ')],
 ]
 
-sg.Window('Converter', layout).read()
+window = sg.Window('Converter', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
 
 
