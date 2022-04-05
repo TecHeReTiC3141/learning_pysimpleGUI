090c825a88728150c103ad8123b0ead2fe5e7e71
TecHeReTiC <talex73@mail.ru>
2022-04-06 00:17:58 +0500

    New flexible method of gathering data has been improved. Besides, some bugs have been fixed.

--- currency_converter.py
+++ currency_converter.py
@@ -1,13 +1,17 @@
 import PySimpleGUI as sg
-from getting_info_about_currency import source_data, currency
+from getting_info_about_currency import *
+
+sourse_date, curr_data = get_currency_info(['USD', 'EUR', 'PLN', 'UAH', 'JPY'])
+pprint(curr_data)
+
 layout = [
     [sg.Text('Hello, Insert value:', enable_events=True, key='-GREET-'),
      sg.Input(key='-INPUT-')],
-    [sg.Button('Convert!', key='-CONV-'), sg.Spin(list(currency.keys()), key='-CURRENCY-'), sg.Spin(['to rouble', 'from rouble'], key='-TYPE-')],
-    [sg.Text('Input: ; Result: ', key='-RESULT-'), sg.Button('Reset', key='-RESET-')],
+    [sg.Button('Convert!', key='-CONV-'), sg.Spin(list(curr_data.keys()), key='-CURRENCY-'), sg.Spin(['to rouble', 'from rouble'], key='-TYPE-')],
+    [sg.Text('Input: ; Result: ', key='-RESULT-'), sg.Button('Reset', key='-RESET-'), sg.Text(f'Data gathered in {sourse_date}', background_color='White', text_color='Black')],
 ]
 
-window = sg.Window('Converter', layout)
+window = sg.Window('Currency Converter', layout)
 while True:
     event, values = window.read()
 
@@ -15,15 +19,15 @@ while True:
         break
 
     elif event == '-CONV-':
-        print('In progress')
         try:
             inp = float(values["-INPUT-"])
-            cur_val = currency[values["-CURRENCY-"]]
+            cur_val = curr_data[values["-CURRENCY-"]]
+            window['-RESULT-'].update(visible=True)
             if values["-TYPE-"] == "to rouble":
 
                 window['-RESULT-'].update(f'Input: {inp}; Result: {round(inp * cur_val["Value"] / cur_val["Nominal"], 2)}₽')
             else:
-                window['-RESULT-'].update(f'Input: {inp}; Result: {round(inp / cur_val["Value"] * cur_val["Nominal"], 2)} {cur_val["Name"]}')
+                window['-RESULT-'].update(f'Input: {inp}₽; Result: {round(inp / cur_val["Value"] * cur_val["Nominal"], 2)} {cur_val["Name"]}')
         except Exception as e:
             window['-RESULT-'].update('Wrong input. Please type a number')
             print(e)
@@ -31,7 +35,7 @@ while True:
 
     elif event == '-RESET-':
         print('The result was wiped')
-        window['-RESULT-'].update(f'Input: ; Result: ')
+        window['-RESULT-'].update(visible=False)
 
     elif event == '-GREET-':
         print(values)
