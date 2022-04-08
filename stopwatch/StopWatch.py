b495ba8bfa38a5dd5964849bb413cda15dd69e08
TecHeReTiC <talex73@mail.ru>
2022-04-09 02:27:38 +0500

    Some insignificant changes.

--- stopwatch/StopWatch.py
+++ stopwatch/StopWatch.py
@@ -3,26 +3,33 @@ from time import sleep, time
 
 sg.theme('black')
 
-layout = [
-    [sg.Push(), sg.Image('m_closing_button.png', pad=0, enable_events=True, key='-EXIT-')],
-    [sg.VPush(), ],
-    [sg.Text('0.0', key='-TIME-', font='Young 45')],
-    [
-        sg.Button('Lap', button_color=('white', 'red'), border_width=2),
-        sg.Button('Start', button_color=('white', 'red'), border_width=2, key='-STARTSTOP-')
-    ],
-    [sg.VPush()]
-]
+def create_start_window():
 
-window = sg.Window('StopWatch',
-                   layout,
-                   size=(250, 250),
-                   no_titlebar=True,
-                   element_justification='center')
+    layout = [
+        [sg.Push(), sg.Image('m_closing_button.png', pad=0, enable_events=True, key='-EXIT-')],
+        [sg.VPush(), ],
+        [sg.Text('0.0', key='-TIME-', font='Young 45')],
+        [
+            sg.Button('Lap', button_color=('white', 'red'), border_width=2, key='-LAP-', visible=False),
+            sg.Button('Start', button_color=('white', 'red'), border_width=2, key='-STARTSTOP-')
+        ],
+        [sg.VPush()]
+    ]
+
+    window = sg.Window('StopWatch',
+                       layout,
+                       size=(250, 250),
+                       no_titlebar=True,
+                       element_justification='center')
+
+    return window
+
+
 beg_time, active = time(), False
+window = create_start_window()
 
 while True:
-    event, values = window.read(timeout=100)
+    event, values = window.read(timeout=10)
 
     if event in [sg.WIN_CLOSED, '-EXIT-']:
         break
@@ -31,11 +38,18 @@ while True:
         if window['-STARTSTOP-'].get_text() == 'Start':
             window['-STARTSTOP-'].update('Reset')
             beg_time, active = time(), True
-            window['-TIME-'].update(round(time() - beg_time, 1))
+            window['-LAP-'].update(visible=True)
+
 
         else:
             window['-STARTSTOP-'].update('Start')
             active = False
+            window['-TIME-'].update(round(0., 1))
+            window['-LAP-'].update(visible=False)
+
+    elif event == '-LAP-':
+        pass
+        # TODO implement lap button and updating window
 
     if active:
         window['-TIME-'].update(round(time() - beg_time, 1))
