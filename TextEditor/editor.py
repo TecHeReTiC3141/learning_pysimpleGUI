494d879e4fa4fc481e212ede48a559ea57f2bf9a
TecHeReTiC <talex73@mail.ru>
2022-04-11 02:13:50 +0500

    Changing theme has been implemented.

--- TextEditor/editor.py
+++ TextEditor/editor.py
@@ -3,25 +3,31 @@ from string import digits, ascii_letters
 
 sg.theme('dark')
 
-def stats_window(text: str) -> sg.Window:
-    symbl = len([i for i in text if digits + ascii_letters])
+themes = ['menu', ['DarkAmber', 'dark', 'graygraygray', 'random']]
+
+
+def create_main_window(theme: str = 'DarkAmber', content: str = '') -> sg.Window:
+    sg.theme(theme)
     layout = [
-        [sg.Text(f'Text stats: {len(text.split())} words, {symbl} charachers')]
+        [sg.Menu([
+            ['File', ['Open', 'Save', '---', 'Exit']],
+            ['Edit and tools', [themes, 'Stats', 'LowerCase', 'UpperCase']],
+            ['Insert', ['Time']]
+        ], key='-MENU-')],
+        [sg.Text('Untitled', key='-FILENAME-')],
+        [sg.Multiline(default_text=content, expand_x=True, expand_y=True,
+                      pad=((5, 5), (5, 15)), key='-TEXT-', no_scrollbar=True)]
     ]
-    return sg.Window('Stats', layout)
 
-layout = [
-    [sg.Menu([
-        ['File', ['Open', 'Save', '---', 'Exit']],
-        ['Edit and tools', ['Stats', 'LowerCase', 'UpperCase']],
-        ['Insert', ['Time']]
-    ], key='-MENU-')],
-    [sg.Text('Untitled', key='-FILENAME-')],
-    [sg.Multiline(expand_x=True, expand_y=True, pad=((5, 5), (5, 15)), key='-TEXT-')]
+    return sg.Window('TecEditor', layout, size=(400, 600))
 
-]
 
-window = sg.Window('TecEditor', layout, size=(400, 600))
+def stats_window(text: str):
+    symbl = len([i for i in text if i in digits + ascii_letters])
+    sg.popup(f'Text stats: {len(text.split())} words, {symbl} characters', title='Stats')
+
+
+window = create_main_window()
 
 while True:
     event, values = window.read()
@@ -30,9 +36,9 @@ while True:
         break
 
     elif event == 'Stats':
-        new_window = stats_window(window['-TEXT-'].get())
-        new_window.read()
+        stats_window(values['-TEXT-'])
 
-
+    elif event in themes[1]:
+        window = create_main_window(event, values['-TEXT-'])
 
 window.close()
