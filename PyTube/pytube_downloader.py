4d03063cf2146a7845a1aa3e760b69dda396f392
TecHeReTiC <talex73@mail.ru>
2022-04-21 01:46:06 +0500

    Layout for downloading tab has been created.

--- PyTube/pytube_downloader.py
+++ PyTube/pytube_downloader.py
@@ -1,28 +1,32 @@
 import PySimpleGUI as sg
 from pytube import YouTube
 
-sg.theme('dark')
+sg.theme('Darkred')
 
 info_tab = [
     [sg.Text('Title: ', key='-TITLE-')],
-    [sg.Text('Views: ', key='-VIEWS-'),],
-    [sg.Text('Length: ', key='-LEN-'),],
-    [sg.Text('Author: ', key='-AUTHOR-'),],
+    [sg.Text('Views: ', key='-VIEWS-'), ],
+    [sg.Text('Length: ', key='-LEN-'), ],
+    [sg.Text('Author: ', key='-AUTHOR-'), ],
     [sg.Text('Description: ', key='-DESCR-'),
      sg.Multiline(default_text='Choose video to download', no_scrollbar=True, disabled=True)],
 ]
 
 down_tab = [
-    []
+    [sg.Frame('Best quality', [[sg.Button('Download', key='-DOWNBEST-')]])],
+    [sg.Frame('Worst quality', [[sg.Button('Download', key='-DOWNWORST-')]])],
+    [sg.Frame('Audio quality', [[sg.Button('Download', key='-AUDIOONLY-')]])],
+    [sg.Progress(100, expand_x=True, size=(20, 20), pad=((10, 10), (5, 5)))]
 ]
 
+# TODO set keys to elements
 
 layout = [
-    [sg.Text('YouTube Downloader', font='Young 45')],
+    [sg.Text('YouTube Downloader', font='Young 35')],
     [sg.HorizontalSeparator()],
     [sg.TabGroup([[
-        sg.Tab('info', info_tab),
-        sg.Tab('info', down_tab),
+        sg.Tab('info', info_tab, expand_x=True),
+        sg.Tab('download', down_tab, expand_x=True),
     ]])]
 ]
 
