c0e357a09091daccd911b2a0f3b2d0e4c969b01e
TecHeReTiC <talex73@mail.ru>
2022-04-25 00:26:23 +0500

    Basic layout is nearly ready.

--- WeatherPredictor/weather_app.py
+++ WeatherPredictor/weather_app.py
@@ -10,20 +10,22 @@ def update_image(filename: str, key: str, size: tuple):
 
 
 im_col = sg.Column([
-    [sg.Image('', size=(300, 300), key='-SUNNY-')]
+    [sg.Image('', size=(300, 300), key='-WEATHER-', background_color='#FFFFFF')]
 ])
 
 forecast_col = sg.Column([
-    [sg.Text('Chusovoy', background_color='red', font='Frank 35', text_color='grey')]
+    [sg.Text('Chusovoy', background_color='red', font='Frank 35', text_color='grey', pad=1, key='-LOC-', visible=False)],
+    [sg.Text('Time', background_color='white', font='Frank 35', text_color='black', pad=1, key='-TIME-', visible=False)],
+    [sg.Text('Temperature', background_color='red', font='Frank 35', text_color='grey', pad=1, key='-TEMP-', visible=False)]
 ])
 
 layout = [
-    [sg.Input(key='-INPUT-', expand_x=True), sg.Button('Submit')],
+    [sg.Input(key='-INPUT-', expand_x=True), sg.Button('Submit', border_width=2)],
     [im_col, sg.VerticalSeparator(), forecast_col],
 ]
 
 window = sg.Window('Weather Forecast', layout, finalize=True)
-update_image('images/sunny.png', '-SUNNY-', (300, 300))
+update_image('images/sunny.png', '-WEATHER-', (300, 300))
 
 while True:
 
@@ -31,5 +33,11 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
+
+    elif event == 'Submit':
+        window['-LOC-'].update(visible=True)
+        window['-TIME-'].update(visible=True)
+        window['-TEMP-'].update(visible=True)
+        update_image('images/sunny.png', '-WEATHER-', (350, 350))
 
 window.close()
