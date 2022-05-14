7706029aa26aea412ae134385df9b459de828cc6
TecHeReTiC <talex73@mail.ru>
2022-05-14 15:42:45 +0500

    Basic queries have been implemented.

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,25 +1,28 @@
 import PySimpleGUI as sg
 import pandas as pd
 
-data, headings = pd.DataFrame(), []
+data, queried = pd.DataFrame(), pd.DataFrame()
+query = False
 sg.theme('DarkAmber')
 sg.set_options(font='Ubuntu 15')
-formats = ['format', ['*.csv', '*.xlsx']]
+from_formats = ['format', ['*.csv', '*.xlsx']]
+to_formats = ['formats', ['to .csv', 'to .xlsx']]
 cur_format = '*.csv'
 
-def create_window(data: pd.DataFrame) -> sg.Window:
 
-    table = sg.Table(values=data.values.tolist(), headings=data.columns.values.tolist(), key='-DATA-', expand_x=True, auto_size_columns=True)
+def create_window(data: pd.DataFrame) -> sg.Window:
+    table = sg.Table(values=data.values.tolist(),
+                     headings=data.columns.values.tolist(), key='-DATA-', expand_x=True, auto_size_columns=True)
 
     data_tab = sg.Tab('Data',
                       layout=[
-
                           [sg.Text('Write query to data:')],
-                          [sg.Input(key='-MAKEQUERY-', expand_x=True), sg.Button('Query')]
+                          [sg.Input(key='-MAKEQUERY-', expand_x=True), sg.Button('Query'), sg.Button('Reset')]
                       ], key='-DATATAB-', expand_x=True)
 
     load_tab = sg.Tab('Load', layout=[
-        [sg.Button('Load data', right_click_menu=formats), sg.Text(f'Current format: {cur_format}', key='-CURFORM-')]
+        [sg.Button('Load data', right_click_menu=from_formats), sg.Text(f'Current format: {cur_format}', key='-CURFORM-')],
+        [sg.Button('Save data', right_click_menu=to_formats), sg.Text(f'Current format: {cur_format}', key='-CURFORM-')]
     ], expand_x=True)
 
     layout = [
@@ -27,13 +30,13 @@ def create_window(data: pd.DataFrame) -> sg.Window:
         [sg.TabGroup([[data_tab, load_tab]])],
 
         [sg.HorizontalSeparator()],
-        [sg.Text('Data')],
+        [sg.Text(f'Data {data.shape[0]} rows x {data.shape[1]} columns')],
         [table],
     ]
 
-
-    window = sg.Window('DataLoader', layout=layout, element_justification='center')
+    window = sg.Window('DataLoader', layout=layout, element_justification='center', finalize=True)
     return window
+
 
 window = create_window(data)
 
@@ -43,7 +46,7 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    elif event in formats[1]:
+    elif event in to_formats[1]:
         cur_format = event
         window['-CURFORM-'].update(f'Current format: {cur_format}')
 
@@ -56,13 +59,31 @@ while True:
 
             window = create_window(data)
             print(window)
-            print(headings, data.columns.values)
+            print(data.columns.values)
             print(window['-DATA-'].Values[0], len(window['-DATA-'].Values))
         except Exception as e:
             print(e)
             sg.popup(str(e))
 
+
     elif event == 'Query':
-        pass
+        try:
+            if len(data):
+                query = values['-MAKEQUERY-']
+                queried = data.query(query)
+
+                window.close()
+                window = create_window(queried)
+                window['-MAKEQUERY-'].update(query)
+            else:
+                sg.popup('Please load some some first')
+        except Exception as e:
+            print(e)
+            sg.popup(str(e))
+
+
+    elif event == 'Reset':
+        window.close()
+        window = create_window(data)
 
 window.close()
