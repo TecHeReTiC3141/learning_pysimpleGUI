5c54b8daed819fc9ee68372f4e9f588fea5e367f
TecHeReTiC <talex73@mail.ru>
2022-05-24 22:10:16 +0500

    Tab for numeric features stats seems to be implemented.

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -3,17 +3,21 @@ import pandas as pd
 import numpy as np
 
 data, queried = pd.DataFrame(), pd.DataFrame()
-query = False
+pd.set_option("display.precision", 3)
+
+is_query = False
 sg.theme('DarkAmber')
 sg.set_options(font='Ubuntu 15')
 from_formats = ['format', ['*.csv', '*.xlsx']]
 to_formats = ['formats', ['to .csv', 'to .xlsx']]
 cur_format = '*.csv'
 
-
+# TODO create tab with data stats (pandas.describe)
+# TODO implement saving of data and queries
+# TODO learn more about getting and changing certain rows
 def create_window(data: pd.DataFrame) -> sg.Window:
     table = sg.Table(values=np.hstack((np.array(range(1, data.shape[0] + 1)).reshape((data.shape[0], 1)), data.values)).tolist(),
-                     headings=['Id'] + data.columns.values.tolist(), max_col_width=45,
+                     headings=['Id'] + data.columns.values.tolist(), max_col_width=35,
                      key='-DATA-', expand_x=True, auto_size_columns=True, enable_events=True)
 
     data_tab = sg.Tab('Data',
@@ -27,9 +31,33 @@ def create_window(data: pd.DataFrame) -> sg.Window:
         [sg.Button('Save data', right_click_menu=to_formats), sg.Text(f'Current format: {cur_format}', key='-CURFORM-')]
     ], expand_x=True)
 
+    if data.shape[1] > 0:
+        numer_descr = data.describe().round(3).T
+        print(numer_descr)
+
+        print(numer_descr.index.to_numpy().reshape(numer_descr.shape[0], 1), numer_descr.values)
+
+        num_stats_tab = sg.Tab('Numeric Stats',
+                           layout=[
+                               [sg.Text('Numeric features description')],
+                               [sg.HorizontalSeparator()],
+                               [sg.Table(values=np.hstack([numer_descr.index.to_numpy().reshape(numer_descr.shape[0], 1),
+                                                           numer_descr.values]).tolist(),
+                                         headings=['Column'] + numer_descr.columns.tolist())]
+                           ])
+
+        tabs = sg.TabGroup([
+            [data_tab, load_tab],
+            [num_stats_tab]
+        ])
+    else:
+        tabs = sg.TabGroup([
+            [data_tab, load_tab],
+        ])
+
     layout = [
         [sg.Text('DataLoader', font='Ubuntu 25 italic')],
-        [sg.TabGroup([[data_tab, load_tab]])],
+        [tabs],
 
         [sg.HorizontalSeparator()],
         [sg.Text(f'Data {data.shape[0]} rows x {data.shape[1]} columns')],
@@ -52,7 +80,6 @@ while True:
         cur_format = event
         window['-CURFORM-'].update(f'Current format: {cur_format}')
 
-
     elif event == 'Load data':
         try:
             filepath = sg.popup_get_file('', no_window=True, )
@@ -67,7 +94,6 @@ while True:
             print(e)
             sg.popup(str(e))
 
-
     elif event == 'Query':
         try:
             if len(data):
@@ -77,6 +103,7 @@ while True:
                 window.close()
                 window = create_window(queried)
                 window['-MAKEQUERY-'].update(query)
+                is_query = True
             else:
                 sg.popup('Please load some some first')
         except Exception as e:
@@ -87,6 +114,7 @@ while True:
     elif event == 'Reset':
         window.close()
         window = create_window(data)
+        is_query = False
 
     elif event == '-DATA-':
         print(values[event])
